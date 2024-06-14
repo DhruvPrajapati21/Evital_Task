9bfdeceb21f47e427aa3354cdedf43fd2aa570b1
DhruvPrajapati21 <chetanbhai.dhruv@gmail.com>
2024-06-14 14:31:43 +0530

    My First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Evital Task',
+        title: 'EvitalRx Task',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(),
         home: Splashscreen()
